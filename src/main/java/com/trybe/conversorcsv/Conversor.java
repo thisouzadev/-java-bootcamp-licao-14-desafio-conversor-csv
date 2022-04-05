272c0b9da49b8efe992ae9a46bcf9625d793d2f0
Thiago Souza <78744052+thisouzadev@users.noreply.github.com>
2022-04-05 13:21:03 -0300

    feat:  read.csv

--- src/main/java/com/trybe/conversorcsv/Conversor.java
+++ src/main/java/com/trybe/conversorcsv/Conversor.java
@@ -1,6 +1,8 @@
 package com.trybe.conversorcsv;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Conversor {
@@ -9,8 +11,8 @@ public class Conversor {
    * Função utilizada apenas para validação da solução do desafio.
    *
    * @param args Não utilizado.
-   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
-   *                     gravar os arquivos de saída.
+   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou gravar os
+   *         arquivos de saída.
    */
   public static void main(String[] args) throws IOException {
     File pastaDeEntradas = new File("./entradas/");
@@ -19,17 +21,36 @@ public class Conversor {
   }
 
   /**
-   * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados
-   * na pasta de saídas, deixando os arquivos originais inalterados.
+   * Converte todos os arquivos CSV da pasta de entradas. Os resultados são gerados na pasta de
+   * saídas, deixando os arquivos originais inalterados.
    *
    * @param pastaDeEntradas Pasta contendo os arquivos CSV gerados pela página web.
-   * @param pastaDeSaidas Pasta em que serão colocados os arquivos gerados no formato
-   *                      requerido pelo subsistema.
+   * @param pastaDeSaidas Pasta em que serão colocados os arquivos gerados no formato requerido pelo
+   *        subsistema.
    *
-   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou
-   *                     gravar os arquivos de saída.
+   * @throws IOException Caso ocorra algum problema ao ler os arquivos de entrada ou gravar os
+   *         arquivos de saída.
    */
   public void converterPasta(File pastaDeEntradas, File pastaDeSaidas) throws IOException {
     // TODO: Implementar.
+    if (!pastaDeSaidas.exists()) {
+      pastaDeSaidas.mkdir();
+    }
+
+    FileReader leitorArquivo = null;
+    BufferedReader bufferedLeitor = null;
+    for (File file : pastaDeEntradas.listFiles()) {
+      leitorArquivo = new FileReader(file);
+      bufferedLeitor = new BufferedReader(leitorArquivo);
+
+      String conteudoLinha = bufferedLeitor.readLine();
+      while (conteudoLinha != null) {
+        System.out.println(conteudoLinha);
+        conteudoLinha = bufferedLeitor.readLine();
+      }
+      leitorArquivo.close();
+      bufferedLeitor.close();
+    }
+
   }
 }
